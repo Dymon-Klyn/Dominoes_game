ba23010d9dcb6dfaa8b4b69094e7fa6384f1a948
Daniel <danyalternate@gmail.com>
2020-02-19 20:03:56 -0500

    fixed bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 class Game:
     def __init__(self):
-        self.dominoes = itertools.combinations_with_replacement(range(10), 2)
+        self.dominoes = [x for x in itertools.combinations_with_replacement(range(10), 2)]
 
 g = Game()
 print(g.dominoes)
